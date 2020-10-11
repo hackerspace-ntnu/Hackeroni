4399a4cd043460a401a3063152acc07120c03058
Glen H.M. Sørensen <gsorensen1201@gmail.com>
2020-10-11 14:50:29 +0200

    Fixed the bug that launches two instances of a minigame (#17)

--- Assets/Scripts/SimpleBootstrap.cs
+++ Assets/Scripts/SimpleBootstrap.cs
@@ -8,41 +8,49 @@ using TMPro;
 public class SimpleBootstrap : MonoBehaviour
 {
     public TMP_Text textObject;
+
+    private bool gameLaunched = false;
     void Start()
     {
     }
 
     void OnSceneEnd(MinigameScene.Outcome outcome)
     {
+        gameLaunched = false;
         Debug.LogFormat("Your highscore was: {0}", outcome.highscore);
     }
 
     // Update is called once per frame
     public void OnButtonPress(int buttonIndex)
     {
-        switch (buttonIndex)
+        if (!gameLaunched)
         {
-            case 0:
-                MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd); 
-                break;
-            case 1:
-                MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
-                break;
-            // TODO Add new games here as they are made
-            case 2:
-                // MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd); 
-                break;
-            case 3:
-                // MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
-                break;
-            case 4:
-                // MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd); 
-                break;
-            case 5:
-                // MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
-                break;
-            default:
-                break;
+            gameLaunched = true;
+
+            switch (buttonIndex)
+            {
+                case 0:
+                    MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd); 
+                    break;
+                case 1:
+                    MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
+                    break;
+                // TODO Add new games here as they are made
+                case 2:
+                    // MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd); 
+                    break;
+                case 3:
+                    // MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
+                    break;
+                case 4:
+                    // MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd); 
+                    break;
+                case 5:
+                    // MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
+                    break;
+                default:
+                    break;
+            }           
         }
     }
 }
