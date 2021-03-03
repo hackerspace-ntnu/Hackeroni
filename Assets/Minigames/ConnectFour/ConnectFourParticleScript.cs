dc06a271cb0404f54b2f59b61d8ead6ff909ae66
johnnyhn <johnnyhn@stud.ntnu.no>
2021-03-03 14:05:41 +0100

    Made Game (#32)

---
+++ Assets/Minigames/ConnectFour/ConnectFourParticleScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectFourParticleScript : MonoBehaviour
+{
+    float Timer;
+    float TotalTime;
+    Vector3 From;
+    Vector3 To;
+    bool Move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Move == true)
+        {
+            MoveToUpdate();
+        }
+    }
+
+    public void MoveToStart(Vector3 To, float Time)
+    {
+        Move = true;
+        this.To = To;
+        TotalTime = Time;
+        From = transform.position;
+        Timer = 0f;
+
+        print(To);
+    }
+
+    void MoveToUpdate()
+    {
+        Timer += Time.deltaTime;
+
+        if(Timer >= TotalTime)
+        {
+            Timer = TotalTime;
+
+            Move = false;
+            GameObject.Find("Engine").GetComponent<ConnectFourEngine>().UpdateScoreText();
+        }
+
+        Vector3 NextPos = From + ((To - From) * (Timer/TotalTime));
+        transform.position = NextPos;
+    }
+}
---
+++ Assets/Minigames/ConnectFour/ConnectFourTokenScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectFourTokenScript : MonoBehaviour
+{
+    public int x;
+    public int y;
+    public int Player;
+
+    public List<GameObject> ParticleSystems = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculatePosition();
+    }
+
+    public void CalculatePosition()
+    {
+        x = Mathf.RoundToInt(transform.localPosition.x);
+        y = Mathf.RoundToInt(transform.localPosition.y);
+    }
+
+    public void BreakParticles()
+    {
+        GameObject Particles = null;
+
+        if(Player == 1)
+        {
+            Particles = Instantiate(ParticleSystems[0]);
+        }
+        else if(Player == 2)
+        {
+            Particles = Instantiate(ParticleSystems[1]);
+        }
+
+        Particles.transform.position = transform.position;
+    }
+
+    public void BreakPointParticles()
+    {
+        GameObject Particles = null;
+
+        if (Player == 1)
+        {
+            Particles = Instantiate(ParticleSystems[2]);
+            Particles.transform.position = transform.position;
+            //Vector3 pos = Camera.main.ViewportToWorldPoint(GameObject.Find("Engine").GetComponent<ConnectFourEngine>().PlayerPointsText.transform.position);
+            Particles.GetComponent<ConnectFourParticleScript>().MoveToStart(new Vector3(-1.685027f, 17.84167f, -2.855676f), 0.5f);
+        }
+        else if (Player == 2)
+        {
+            Particles = Instantiate(ParticleSystems[3]);
+            Particles.transform.position = transform.position;
+            //Vector3 pos = Camera.main.ViewportToWorldPoint(GameObject.Find("Engine").GetComponent<ConnectFourEngine>().OpponentPointsText.transform.position);
+            Particles.GetComponent<ConnectFourParticleScript>().MoveToStart(new Vector3(2.178409f, 17.84167f, -2.855647f), 0.5f);
+        }
+    }
+}
