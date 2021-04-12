8f772cf378b459e175abcc456922bc479bc8c817
Thomas <shuttler67@gmail.com>
2021-04-12 17:26:27 +0200

    Shuttler67/issue47 (#55)
    * Add music to main menu.
    
    Still need to fix music with the shop.
    
    * Made mainmenu music go to shop scene as well

--- Assets/Scripts/SimpleBootstrap.cs
+++ Assets/Scripts/SimpleBootstrap.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Assets.Scripts;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SimpleBootstrap : MonoBehaviour
 {
@@ -11,8 +12,20 @@ public class SimpleBootstrap : MonoBehaviour
 
     private bool gameLaunched = false;
     private int launchedGameId = 0;
-    
+
     private int hackeronisBeforeMinigameIsLaunched = 0;
+
+    public AudioSource musicSourcePrefab;
+    private static AudioSource theImmortalMusicSource = null;
+
+    void Awake()
+    {
+        if (theImmortalMusicSource != null)
+            return;
+        theImmortalMusicSource = Instantiate(musicSourcePrefab);
+        DontDestroyOnLoad(theImmortalMusicSource);
+        theImmortalMusicSource.Play();
+    }
 
     void Start()
     {
@@ -24,6 +37,11 @@ public class SimpleBootstrap : MonoBehaviour
         gameLaunched = false;
         Screen.orientation = ScreenOrientation.Portrait;
         StartCoroutine(HackeroniEarnedAnimation());
+        if (theImmortalMusicSource != null)
+        {
+            theImmortalMusicSource.time = 0;
+            theImmortalMusicSource.Play();
+        }
     }
 
     // Update is called once per frame
@@ -35,23 +53,28 @@ public class SimpleBootstrap : MonoBehaviour
             launchedGameId = buttonIndex;
             hackeronisBeforeMinigameIsLaunched = PlayerPrefs.GetInt("Hackeronis", 0);
 
+            if (theImmortalMusicSource != null)
+            {
+                theImmortalMusicSource.Pause();
+            }
+
             switch (buttonIndex)
             {
                 case 0:
-                    MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd); 
+                    MinigameScene.LoadMinigameScene("MatchTwo", OnSceneEnd);
                     break;
                 case 1:
-                    MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
+                    MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd);
                     break;
                 case 2:
-                    MinigameScene.LoadMinigameScene("TrickyTrumpTest", OnSceneEnd); 
+                    MinigameScene.LoadMinigameScene("TrickyTrumpTest", OnSceneEnd);
                     break;
                 case 3:
-                    MinigameScene.LoadMinigameScene("TiltyHacker", OnSceneEnd); 
+                    MinigameScene.LoadMinigameScene("TiltyHacker", OnSceneEnd);
                     break;
                 // TODO Add new games here as they are made
                 case 4:
-                    MinigameScene.LoadMinigameScene("Soup_falls", OnSceneEnd); 
+                    MinigameScene.LoadMinigameScene("Soup_falls", OnSceneEnd);
                     break;
                 case 5:
                     // MinigameScene.LoadMinigameScene("Rythmic_fisk", OnSceneEnd); 
@@ -60,33 +83,34 @@ public class SimpleBootstrap : MonoBehaviour
                     gameLaunched = false;
                     launchedGameId = 0;
                     break;
-            }           
+            }
         }
     }
-    
+
     public IEnumerator HackeroniEarnedAnimation()
     {
         var previousHackeronis = hackeronisBeforeMinigameIsLaunched;
         var currentHackeronis = PlayerPrefs.GetInt("Hackeronis", 0);
 
-        if(previousHackeronis >= currentHackeronis) {
+        if (previousHackeronis >= currentHackeronis)
+        {
             //Special case maybe?
-            hackeroniText.text = currentHackeronis.ToString(); 
+            hackeroniText.text = currentHackeronis.ToString();
             yield break;
         }
 
         float animationTime = 1.5f;
         float timer = 0;
 
-        while(timer <= animationTime)
+        while (timer <= animationTime)
         {
             int intermediateValue = Mathf.RoundToInt((currentHackeronis - previousHackeronis) * timer / animationTime + previousHackeronis);
-            hackeroniText.text = intermediateValue.ToString(); 
+            hackeroniText.text = intermediateValue.ToString();
             timer += Time.deltaTime;
             yield return null;
         }
 
-        hackeroniText.text = currentHackeronis.ToString(); 
+        hackeroniText.text = currentHackeronis.ToString();
         hackeronisBeforeMinigameIsLaunched = previousHackeronis;
     }
 }
